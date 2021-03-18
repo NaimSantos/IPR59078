665b7e4a78979f3efbd7849227c41457cf904058
NaimSantos <naimsantos45@gmail.com>
2021-03-18 18:46:31 -0300

    Remoção de versões adicionais de aproximações por diferenças finitas

--- finite_diff.h
+++ finite_diff.h
@@ -5,62 +5,33 @@
 
 #include <functional>
 
-//Diferenças finitas avançada para primeira derivada.
+//Diferenças finitas avançada para a primeira derivada:
 double fdiff_a_1st(double x, double h, std::function<double (double)> f){
 	double res = (f(x+h) - f(x)) / h;
 	return res;
 }
-double fdiff_a_1st_v2(double x, double h, std::function<double (double)> f){
-	double res = (-f(x+2*h) + 4*f(x+h) - 3*f(x)) / (2*h);
-	return res;
-}
-
-//Diferenças finitas avançada para segunda derivada.
+//Diferenças finitas avançada para a segunda derivada:
 double fdiff_a_2nd(double x, double h, std::function<double (double)> f){
 	double res = (f(x+2*h) - 2*f(x+h) + f(x)) / (h*h);
 	return res;
 }
-double fdiff_a_2nd_v2(double x, double h, std::function<double (double)> f){
-	double res = (-f(x+3*h) - 4*f(x+2*h) - 5*f(x+h) + 2*f(x)) / (h*h);
-	return res;
-}
-
-//Diferenças finitas recuada para primeira derivada.
+//Diferenças finitas recuada para a primeira derivada:
 double fdiff_r_1st(double x, double h, std::function<double (double)> f){
 	double res = (f(x) - f(x-h)) / h;
 	return res;
 }
-double fdiff_r_1st_v2(double x, double h, std::function<double (double)> f){
-	double res = (3*f(x) - 4*f(x-h) + f(x-2*h)) / (2*h);
-	return res;
-}
-
-//Diferenças finitas recuada para segunda derivada:
+//Diferenças finitas recuada para a segunda derivada:
 double fdiff_r_2nd(double x, double h, std::function<double (double)> f){
 	double res = (f(x) - 2*f(x-h) + f(x-2*h)) / (h*h);
 	return res;
 }
-double fdiff_r_2nd_v2(double x, double h, std::function<double (double)> f){
-	double res = (2*f(x) - 5*f(x-h) + 4*f(x-2*h) + f(x-3*h)) / (h*h);
-	return res;
-}
-
-//Diferenças finitas centrada  para primeira derivada:
+//Diferenças finitas centrada para a primeira derivada:
 double fdiff_c_1st(double x, double h, std::function<double (double)> f){
 	double res = (f(x+h) - f(x-h)) / (2*h);
 	return res;
 }
-double fdiff_c_1st_v2(double x, double h, std::function<double (double)> f){
-	double res = (-f(x+2*h) + 8*f(x+h) - 8*f(x-h) + f(x-2*h)) / (12*h);
-	return res;
-}
-
-//Diferenças finitas centrada  para segunda derivada:
+//Diferenças finitas centrada para a segunda derivada:
 double fdiff_c_2nd(double x, double h, std::function<double (double)> f){
 	double res = (f(x+h) - 2*f(x) + f(x-h)) / (h*h);
-	return res;
-}
-double fdiff_c_2nd_v2(double x, double h, std::function<double (double)> f){
-	double res = (-f(x+2*h) + 16*f(x+h) - 30*f(x) + 16*f(x-h) - f(x-2*h)) / (12*(h*h));
 	return res;
 }
