70970a534c0e791bdf49da1d66fb1fa99d2e60b6
NaimSantos <naimsantos45@gmail.com>
2021-03-25 09:22:23 -0300

    Implementação (parte 1)
    headers necessários e definição da função espacial f(x)

---
+++ Exercicio 1/exerc1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+
+double f(double x);
+
+constexpr double dx = {0.1};
+constexpr double dt = {0.05};
+
+int main (int argc, char* argv[]){
+	auto x = 0.0;
+	for (int i = 0; i < 100; i++)
+		std:: cout << f(x + 0.01*i) << ' ' ;
+}
+
+double f(double x){
+	return (x <= 0.5) ? (x) : (1 - x);
+}
