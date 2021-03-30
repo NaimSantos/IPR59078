256a3d0e9f194e86f9d483c7e535ef30c3e40700
NaimSantos <naimsantos45@gmail.com>
2021-03-30 11:12:09 -0300

    Parte 1 da implementação do solver implícito

--- Exercicio 1/exerc1.cpp
+++ Exercicio 1/exerc1.cpp
@@ -4,13 +4,16 @@
 #include <iomanip>
 #include <vector>
 
+using std::vector;
+
 //#include "gsiedel.hpp"
-//#include "tdma.hpp"
+#include "tdma.hpp"
 
 double f(double x);
-//void linspacefill(std::vector<double>& Vec, const int Num, const double xi = 0.0, const double xf = 1.0);
-void explictsolver(std::vector<double>& Temp, const double r = 0.5);
-void printvec(const std::vector<double>& Vec);
+//void linspacefill(vector<double>& Vec, const int Num, const double xi = 0.0, const double xf = 1.0);
+void explictsolver(vector<double>& Temp, const double r = 0.5, const int tempo = 10);
+void implicitsolver(vector<double>& U, const vector<double>& a, const vector<double>& b, const vector<double>& c, const int tempo = 10);
+void printvec(const vector<double>& Vec);
 
 
 //Dados do domínio:
@@ -19,8 +22,8 @@ constexpr double dt {1};	//passo de tempo
 constexpr double L {1.0};	//comprimento total do domínio
 constexpr double ti {0.0};	//tempo inicial da simulação
 constexpr double tf {100.0};	//tempo final de simulação
-constexpr auto N  = L/dx + 1; //número de nós da malha (intervalos + 1)
-constexpr double I = (tf - ti) / dt; //número de passos de tempo
+constexpr auto N  = L/dx + 1;	//número de nós da malha (intervalos + 1)
+constexpr double I = (tf - ti) / dt;	//número de passos de tempo
 
 //Dados do problema:
 constexpr double kappa {0.6};
@@ -34,34 +37,65 @@ int main (int argc, char* argv[]){
 
 	const auto alfa = kappa / (rho * cp);
 	const auto r = alfa * dt / (dx * dx);
-	std::cout << "dt = " << dt << ", dx = " << dx  << ", alfa = " << alfa << ", CFL (r) = " << r << std::endl;
+	//std::cout << "dt = " << dt << ", dx = " << dx  << ", alfa = " << alfa << ", CFL (r) = " << r << std::endl;
 
+	const auto rtest = 0.5;
 
-	std::vector<double> Temperature(N, 0.0);
+	// -----------------Solução 1 ---------------------//
+	vector<double> Temperature(N, 0.0);
 
 	//Preencher os temperaturas iniciais a partir da função fornecida no problema:
 	auto n = Temperature.size();
 	for (int i = 0; i < n; i++){
 		Temperature[i] = f(i*dx);
 	}
+	auto T2 = Temperature; //copia para testar via TDMA
 
-	printvec(Temperature);
-	for (int i = 1; i < 100; i++){
-		explictsolver(Temperature, 0.5);
-		printvec(Temperature);
-	}
+	//Invocar o solver explícito:
+	std::cout << "\nExibindo solucao explicita:\n";
+	explictsolver(Temperature, rtest, 10);
+
+
+	// -----------------Solução 2 ---------------------//
+	vector<double> Dsup (N, (-rtest));	//Diagonal superior, inicializada em -r
+	vector<double> Dmain (N, (1 + 2*rtest));	//Diagonal principal, inicializada em 1 + 2r
+	vector<double> Dinf (N, (-rtest));	//Diagonal inferior, inicializada em -r
+
+
+	//Corrigimos os termos cuja inicialização é diferente:
+	Dmain[0] = Dmain[n-1] = 1.0;
+	Dsup[0] = Dinf[n-1] = 0.0;
+
+	//Invoca o solver implicito:
+	std::cout << "\nExibindo solucao implicita:\n";
+	implicitsolver(T2, Dsup, Dmain, Dinf, 10);
 
 }
-void explictsolver(std::vector<double>& Temp, const double r){
-	for (int i = 1; i < N - 1; i++ ){
-		Temp[i] = Temp[i] + r*(Temp[i-1] - 2.0*Temp[i] + Temp[i+1] );
+
+void explictsolver(vector<double>& Temp, const double r, const int tempo){
+	printvec(Temp);
+	for (int iter = 1; iter < tempo; iter++){
+		for (int i = 1; i < N - 1; i++ ){
+			Temp[i] = Temp[i] + r*(Temp[i-1] - 2.0*Temp[i] + Temp[i+1] );
+		}
+		printvec(Temp);
 	}
 }
+
+void implicitsolver(vector<double>& U, const vector<double>& a, const vector<double>& b, const vector<double>& c, const int tempo){
+	printvec(U);
+	for (int iter = 1; iter < tempo; iter++){
+		tdma_solver(a, b, c, U);
+		printvec(U);
+	}
+}
+
 
 double f(double x){
 	return (x <= 0.5) ? (x) : (1 - x);
 }
-void printvec(const std::vector<double>& Vec){
+
+void printvec(const vector<double>& Vec){
 	for (auto& x : Vec)
 		std::cout << std::setw(10) << std::setprecision(5) << x << ' ';
 	std::cout << std::endl;
@@ -70,7 +104,7 @@ void printvec(const std::vector<double>& Vec){
 
 
 /*
-void linspacefill(std::vector<double>& Vec, const int Num, const double xi, const double xf){
+void linspacefill(vector<double>& Vec, const int Num, const double xi, const double xf){
 	auto h = (xf - xi) / Num;
 	auto n = Vec.size();
 	for (int i = 0; i < n; i++){
--- Exercicio 1/tdma.hpp
+++ Exercicio 1/tdma.hpp
@@ -13,21 +13,23 @@
 	d = termos independentes
 */
 
-void tdma_solver(const std::vector<double>& a, const std::vector<double>& b, std::vector<double>& c, std::vector<double>& d){
+void tdma_solver(const std::vector<double>& a, const std::vector<double>& b, const std::vector<double>& c, std::vector<double>& d){
 	auto n = static_cast<int>(d.size()-1);
 
-	c[0] /= b[0];
+	auto c_temp = c;
+	
+	c_temp[0] /= b[0];
 	d[0] /= b[0];
 
 	for (int i = 1; i < n; i++){
-		c[i] = (c[i] ) / (b[i] - a[i]*c[i-1]);
-		d[i] = (d[i] - a[i]*d[i-1]) / (b[i] - a[i]*c[i-1]);
+		c_temp[i] = (c_temp[i] ) / (b[i] - a[i]*c_temp[i-1]);
+		d[i] = (d[i] - a[i]*d[i-1]) / (b[i] - a[i]*c_temp[i-1]);
 	}
 
-	d[n] = (d[n] - a[n]*d[n-1]) / (b[n] - a[n]*c[n-1]);
+	d[n] = (d[n] - a[n]*d[n-1]) / (b[n] - a[n]*c_temp[n-1]);
 
 	for (int i = n; i-- > 0;){
-		d[i] = d[i] - (c[i]*d[i+1]);
+		d[i] = d[i] - (c_temp[i]*d[i+1]);
 	}
 }
 /*
