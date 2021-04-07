52b43b9dd6cfb67fec878290c22d437e8600e8ef
NaimSantos <naimsantos45@gmail.com>
2021-04-07 19:28:56 -0300

    Padronização

--- Trabalho 1/main.cpp
+++ Trabalho 1/main.cpp
@@ -8,9 +8,9 @@
 
 using std::vector;
 
-void printvec(const vector<double>& Vec);
 void implicit(vector<vector<double>>& A, vector<double>& B, const double r);
 void resumesaving(std::fstream& printer, const vector<double>& B, const int iter);
+void printvec(const vector<double>& Vec);
 void printmatriz(const vector<vector<double>>& A);
 
 // Variáveis do domínio da simulação:
@@ -38,27 +38,27 @@ constexpr auto lambda = (g*dt)/(rho*cp);
 
 
 int main (int argc, char* argv[]){
-	
+
 	vector<vector<double>> A (N, std::vector<double>(N, 0.0));
 	vector<double> B (N, T0);
 
-
 	implicit(A, B, r1);
+
 }
 
 void implicit(vector<vector<double>>& A, vector<double>& B, const double r){
-	
+
 	std::fstream printer {"DadosDeTemperatura.dat", std::ios::app};
 	printer << "Perfil de Temperatura via solver implicito.\n";
 	printer << "i t x T\n";
-	
+
 	int step = 0;
 	double t = ti;
 	printer << step << ' ' << t << ' ';
-		for (double x = 0.0; x <= L; x=x+dx)
+		for (double x = 0.0; x <= L; x = x+dx)
 			printer << x << ' ';
 	printer << T0 << '\n';
-	
+
 	//Preenchemos a matriz A (B já foi preenchido):
 	A[0][0] = -3.0;
 	A[0][1] = 4.0;
@@ -70,7 +70,7 @@ void implicit(vector<vector<double>>& A, vector<double>& B, const double r){
 			A[i][k+2] = -r;
 			k++;
 		}
-    A[N-1][N-3] = 1.0;
+	A[N-1][N-3] = 1.0;
 	A[N-1][N-2] = -4.0;
 	A[N-1][N-1] = gamma;
 
@@ -79,8 +79,8 @@ void implicit(vector<vector<double>>& A, vector<double>& B, const double r){
 	for (step = 1; step < nsteps; step++){
 		// Corrige B:
 		B[0] = 0.0;
-        for (int i = 1; i < N-1; i++){
-            B[i] = B[i] + lambda;
+		for (int i = 1; i < N-1; i++){
+			B[i] = B[i] + lambda;
 		}
 		B[N-1] = (2*h*dx*T0)/kappa;
 		//Resolve o sistema:
@@ -107,21 +107,7 @@ void resumesaving(std::fstream& printer, const vector<double>& B, const int iter
 void printmatriz(const vector<vector<double>>& A){
 	for (int k=0; k < N; k++){
 		for (int w = 0; w < N; w++)
-			std::cout <<  std::setw(10) << std::setprecision(5)  << A[k][w] << ' ';
+			std::cout << std::setw(10) << std::setprecision(5) << A[k][w] << ' ';
 		std::cout << std::endl;
 	}
 }
-/*
-	Teste:
-
-	vector<vector<double>> A { {2, 1, 1},
-							   {3, 5, 2},
-							   {2, 1, 4} };
-	vector<double> B {5, 15, 8};
-	std::cout << "Antes do Gauss Siedel" << std::endl;
-	printvec(B);
-	
-	GS_Solver(A, B);
-	std::cout << "\nDepois do Gauss Siedel" << std::endl;
-	printvec(B);
-*/
