3e061969ebc6f0e1f2226887ea54aa99dff08190
NaimSantos <naimsantos45@gmail.com>
2021-05-09 18:28:18 -0300

    Correções e lista de atividades para fazer

--- Trabalho 2/analytic_heat_equation.cpp
+++ Trabalho 2/analytic_heat_equation.cpp
@@ -11,28 +11,25 @@ void analytic_solver(vector<vector<double>>& A, const vector<double>& B);
 double FourierAdjust(double x, double t, const vector<double>& EVal);
 double int_trapz(double a, double b, const double i);
 double f_init(const double x, const double i, bool onintegral = false);
+double Tfx(const double x);
 double invers_N(const double Bn);
 double eigenvalue(const double Bn);
-double solveBisect(double a0, double b0, std::function<double (double)> ftarget);
-double solveNewton(double x, std::function <double (double)> fnwtn);
+double solveNewton(double x, std::function<double (double)> f);
 double f_diff(double x, double h, std::function<double (double)> f);
-
 void linspace(vector<double>& Vec, const int Num, const double xf = 1.0, const double xi = 0.0);
 void printparameters();
 void savedata(const vector<vector<double>>& T, const vector<double>& X);
 
-// Estimativa para pi:
+
 constexpr auto NPI = 4*std::atan(1);
 
 // Variáveis do domínio da simulação:
-constexpr double L {0.03};                                     // comprimento total da placa
-constexpr double ti {0.0};                                     // tempo inicial da simulação
-constexpr double tf {50.0};                                    // tempo final da simulação
-constexpr auto dt {0.1};                                       // passo de tempo na série de Fourier
-constexpr auto nsteps = static_cast<int>((tf-ti)/dt) + 1;      // número de passos de tempo usando a série de Fourier
-constexpr int N {5};                                           // número de elementos na série de Fourier
-constexpr auto dx {0.0025};                                     // comprimento do intervalo na série de Fourier
-constexpr auto N_i = L/2;                                      // coeficiente na série
+constexpr double L {0.03};                                   // comprimento total da placa
+constexpr double ti {0.0};                                   // tempo inicial da simulação
+constexpr double tf {50.0};                                  // tempo final da simulação
+constexpr auto dt {0.1};                                     // passo de tempo na série de Fourier
+constexpr auto nsteps = static_cast<int>((tf-ti)/dt) + 1;    // número de passos de tempo usando a série de Fourier
+constexpr auto dx {0.0025};                                  // comprimento do intervalo na série de Fourier
 constexpr auto Npoints = L/dx + 1;
 
 // Dados do problema:
@@ -41,18 +38,17 @@ constexpr int rho {600};
 constexpr int cp {1200};
 constexpr double h {15.0};
 constexpr int g {100000};
-constexpr double T0 {20.0};
-constexpr double TL {20.0};
 constexpr double T_inf {20.0};
 constexpr auto alpha = kappa/(rho*cp);
 constexpr auto H2 = h/kappa;
 
 int main (int argc, char* argv[]){
 	printparameters();
+
 	// Solução Analítica
 	vector<vector<double>> T (nsteps, std::vector<double>(Npoints, 0.0));
 	vector<double> X(Npoints, 0.0);
-	linspace(X, Npoints, L, 0.0);       // escreve em D uma distribuição dos N pontos de avaliação
+	linspace(X, Npoints, L, 0.0);       // escreve em X uma distribuição dos Npoints pontos de avaliação
 
 	analytic_solver(T, X);
 }
@@ -62,18 +58,20 @@ void analytic_solver(vector<vector<double>>& T, const vector<double>& X){
 	// B is a 1D-vector with the positions
 
 	// Calcular os autovalores:
-	int nroots = 10;
-	vector<double> EVal (nroots, 0.0);	//array onde nroots autovalores serão armazenados.
+	int nroots = 10;                        // número de auto vetores (truncamento da série de Fourier)
+	vector<double> EVal (nroots, 0.0);      // array onde nroots autovalores serão armazenados.
+
 	EVal[0] = solveNewton(20, eigenvalue);
 	for (int i = 1; i < nroots; i++){
 		EVal[i] =  solveNewton(i*100, eigenvalue);
 	}
-	for (const auto& x : EVal) std::cout << std::setprecision(6) << x << ' ';
+	//for (const auto& x : EVal) std::cout << std::setprecision(6) << x << ' ';
 
 	for (int i = 0; i < nsteps; i++){
 		for (int j = 0; j < Npoints; j++){
 			T[i][j] = FourierAdjust(j*dx, i*dt, EVal);
-			T[i][j] = T[i][j] + f_init(j*dx, 0.0, false);	// A Solução é a soma das soluções
+			// A Solução T(x,t) é a soma das soluções (filtrada + filtro): T(x,t) = T*(x,t) + Tf(x)
+			T[i][j] = T[i][j] + Tfx(j*dx);
 		}
 	}
 	// Salvar os resultados em um arquivo:
@@ -82,13 +80,12 @@ void analytic_solver(vector<vector<double>>& T, const vector<double>& X){
 
 double FourierAdjust(double x, double t, const vector<double>& EVal){
 	double res {0.0}, res1{0.0}, res2{0.0}, res3{0.0}, res4 {0.0};
-
 	auto m = EVal.size(); // número de autovalores usados;
 
-	for (int j = 0; j <= m; j++){
+	for (int j = 0; j < m; j++){
 		auto i = EVal[j];
 		res1 = invers_N(i);
-		res2 = std::exp(- alpha * t * std::pow(i*NPI/L, 2));
+		res2 = std::exp(- alpha * std::pow(i*NPI/L, 2) * t);
 		res3 = std::cos(x*i*NPI/L);
 		res4 = int_trapz(0, L, i);
 
@@ -99,16 +96,14 @@ double FourierAdjust(double x, double t, const vector<double>& EVal){
 
 // Integração numérica pela regra do trapézio
 double int_trapz(double a, double b, const double i){
-	const double h = 0.0001;            // passo
-	const auto L2 = (b - a)/2;          // metade do intervalo (L/2)
-
 	double res {0.0};
-	const auto n = static_cast<int>( std::floor((std::fabs(L2 - a)) / h));
+	const double h = 0.0001;                // passo da integração numérica
+	const auto n = static_cast<int>( std::floor((std::fabs(b - a)) / h));
 
 	for (int k = 0; k < n - 1; k++){
 		res += f_init(a + k*h, i, true);
 	}
-	res += (f_init(a, i, true) + f_init(L2, i, true) ) / 2;
+	res += (f_init(a, i, true) + f_init(b, i, true) ) / 2;
 	res *= h;
 	return res;
 }
@@ -120,6 +115,10 @@ double f_init(const double x, double i, bool onintegral){
 		return res*std::cos(x*i*NPI/L);
 	else
 		return res;
+}
+
+double Tfx(const double x){
+	return (-g*x*x)/(2*kappa) + T_inf + (g*L*L)/(2*kappa) + g*L/h;
 }
 
 double invers_N(const double Bn){
@@ -156,11 +155,11 @@ void linspace(vector<double>& Vec, const int Num, const double xf, const double 
 	}
 }
 
-void printparameters(){	
+void printparameters(){
 	std::cout << "L = " << L << "\tdx = " << dx << "\tNpoints = " << Npoints
-				<< "\ntempo total = " << tf << "\tdt = " << dt	<< "\tnsteps = "
-				<< nsteps << "\nElementos na serie (N) = " << N	<<  std::endl;
-}				
+		<< "\ntempo total = " << tf << "\tdt = " << dt << "\tnsteps = "
+		<< nsteps << std::endl;
+}
 
 void savedata(const vector<vector<double>>& T, const vector<double>& X){
 	std::cout << "\nSaveData function called" << std::endl;
